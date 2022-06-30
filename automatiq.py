f4d66396f4e42c280c1b5aed9944f1706beeeb7a
Kyryo <owenonions@gmail.com>
2022-06-30 08:59:01 +0200

    ini commit

---
+++ automatiq.py
@@ -0,0 +1,33 @@
+import lang
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def modules(name: str = typer.Option("", help="Name of a module.")):
+    """List of modules that are available for deployment."""
+
+    if name in lang.app_modules:
+        typer.echo(lang.app_modules[name])
+
+    elif name == "":
+        for module in lang.app_modules:
+            typer.echo(module)
+
+    else:
+        typer.echo("Module not found")
+
+@app.command()
+def deploy(
+    name: str,
+    version: str,
+    force: bool = typer.Option(..., prompt="Are you sure you want to deploy the module?"),
+):
+    if force:
+        typer.echo(f"Deploying {name} version {version}")
+    else:
+        typer.echo("Deployment process canceled")   
+
+
+if __name__ == "__main__":
+    app()
---
+++ lang.py
@@ -0,0 +1,7 @@
+app_modules = {
+
+    "api": "api app",
+    "core": "core app",
+    "art": "art app",
+}
+
