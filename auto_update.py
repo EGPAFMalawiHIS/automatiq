cda0b098b40c346fd29259b4b3fdd002396c3c80
mnaboti <mnaboti@pedaids.org>
2022-07-18 14:01:40 +0200

    Add describe HEADfunctionality for update_api function

--- auto_update.py
+++ auto_update.py
@@ -1,11 +1,8 @@
 #! /usr/bin/python
 
-#from ipaddress import ip_address
-from ipaddress import ip_address
 import subprocess
 import os
 import fileinput
-import sys
 
 def update_api():
     dir_name1="/var/www/BHT-EMR-API"
@@ -15,7 +12,7 @@ def update_api():
     print ("Checkout to latest tag")
     process = subprocess.run(["git", "checkout", "v4.15.15"], stdout=subprocess.PIPE)
     print ("Describing Head")
-    #process = subprocess.Popen(["git", "describe ", "> HEAD "], stdout=subprocess.PIPE)
+    os.system("git describe HEAD")
     filepath1= "Gemfile.lock"
     if os.path.exists(filepath1):
         os.remove(filepath1)
@@ -23,21 +20,21 @@ def update_api():
         
         print("Can not delete the file as it doesn't exists")
    
-    print ("Installing Local Gems")
+    print ("Installing Local Gems..............")
     os.system("bundle install --local")
-    print ("running bin_update art")
+    print ("running bin_update_art_metadata.sh.............")
     os.system("./bin/update_art_metadata.sh development")
 update_api()
 
 def update_Core():
     dir_name2="/var/www/HIS-Core-release"
-    print ("Changing directory")
+    print ("=>Changing directory..........")
     os.chdir(dir_name2)
-    print ("Checkout to latest tag in Core")
+    print ("=>Checkout to latest tag in Core..........")
     process = subprocess.run(["git", "checkout", "v1.2.7"], stdout=subprocess.PIPE)
-    print ("Describing Head")
-#subprocess.check_output(["git", "describe" "> HEAD"]).strip()
-    print ("Copying config.json.example to config.json")
+    print ("=>Describing Head............")
+    os.system("git describe HEAD")
+    print ("create the configuration file by copying config.json.example to config.json........")
     os.popen('cp config.json.example config.json')
     print ("Configuring IP address and Port in config.json")
     ip_address = input("Enter IP address of the facility: ")
