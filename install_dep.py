d877e1af21c588009e0b6a91dd9d895500149e68
dominickasanga <dominickasanga@gmail.com>
2022-08-22 14:00:03 +0200

    backwards compatibility

--- install_dep.py
+++ install_dep.py
@@ -2,14 +2,14 @@
 import subprocess
 
 # running system update
-PRO_1 = subprocess.run(['sudo', 'apt-get', 'update'], stdout=subprocess.PIPE, text=True)
+PRO_1 = subprocess.run(['sudo', 'apt-get', 'update'],  stdout=subprocess.PIPE)
 
 if PRO_1.returncode == 100:
    print("RUN COMMAND USING 'sudo'")
 
 # installing pip 3 for python3
 if PRO_1.returncode == 0:
-   p2 = subprocess.run(['sudo', 'apt-get', '-y', 'install', 'python3-pip'], stdout=subprocess.PIPE, text=True)
+   p2 = subprocess.run(['sudo', 'apt-get', '-y', 'install', 'python3-pip'], stdout=subprocess.PIPE)
    print(p2.stdout)
 
    if p2.returncode != 0:
@@ -17,7 +17,7 @@ if PRO_1.returncode == 0:
 
    # installing typer
    if p2.returncode == 0:
-      p3 = subprocess.run(['pip3', 'install', 'typer'], stdout=subprocess.PIPE, text=True)
+      p3 = subprocess.run(['pip3', 'install', 'typer'], stdout=subprocess.PIPE)
       print(p3.stdout)
 
       if p3.returncode != 0:
@@ -25,14 +25,14 @@ if PRO_1.returncode == 0:
       
       # installing paramiko
       if p3.returncode == 0:
-         p4 = subprocess.run(['pip3', 'install', '-U', 'pip'], stdout=subprocess.PIPE, text=True)
+         p4 = subprocess.run(['pip3', 'install', '-U', 'pip'], stdout=subprocess.PIPE)
          print(p4.stdout)
 
          if p4.returncode != 0:
             print(p4.stderr)
 
          if p4.returncode == 0:
-            p5 = subprocess.run(['pip3', 'install', 'paramiko'], stdout=subprocess.PIPE, text=True)
+            p5 = subprocess.run(['pip3', 'install', 'paramiko'], stdout=subprocess.PIPE)
             print(p5.stdout)
 
             if p5.returncode != 0:
@@ -40,7 +40,7 @@ if PRO_1.returncode == 0:
 
             #  installing sshpass for rsync
             if p5.returncode == 0:
-               p6 = subprocess.run(['sudo', 'apt', 'install', 'sshpass'], stdout=subprocess.PIPE, text=True)
+               p6 = subprocess.run(['sudo', 'apt', 'install', 'sshpass'], stdout=subprocess.PIPE)
                print(p6.stdout)
 
                if p6.returncode != 0:
